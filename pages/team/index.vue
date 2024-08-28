ef953e339b1fdedefe7e1f33011c13748823e565
arnonr <arnonr@kmutnb.ac.th>
2024-08-28 13:54:16 +0700

    update

--- pages/team/index.vue
+++ pages/team/index.vue
@@ -629,7 +629,7 @@
                                                     </div>
                                                 </div>
                                             </div>
-                                            <!-- หัวหน้า สำนักงาน -->
+                                            <!-- อ.วัชรินทร์ -->
                                             <div
                                                 class="col-md-12"
                                                 style="
@@ -738,6 +738,123 @@
                                                                 {{
                                                                     dp
                                                                         .teamItems[4]
+                                                                        .position_level
+                                                                }}
+                                                            </span>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                              <!-- หัวหน้า สำนักงาน -->
+                                              <div
+                                                class="col-md-12"
+                                                style="
+                                                    margin-top: 20px !important;
+                                                "
+                                            >
+                                                <div
+                                                    class="text-center row justify-content-md-center"
+                                                >
+                                                    <div
+                                                        class="team__item-10 col-lg-3 mt-20"
+                                                    >
+                                                        <div
+                                                            class="team__thumb-10 p-relative m-img"
+                                                        >
+                                                            <nuxt-link
+                                                                href="/team-details"
+                                                            >
+                                                                <img
+                                                                    :src="
+                                                                        dp
+                                                                            .teamItems[5]
+                                                                            .team_file
+                                                                    "
+                                                                    alt=""
+                                                                />
+                                                            </nuxt-link>
+
+                                                            <div
+                                                                class="team__contact-overlay"
+                                                            >
+                                                                <div
+                                                                    class="team__contact-top"
+                                                                >
+                                                                    <span>
+                                                                        <svg
+                                                                            width="52"
+                                                                            height="53"
+                                                                            viewBox="0 0 52 53"
+                                                                            fill="none"
+                                                                            xmlns="http://www.w3.org/2000/svg"
+                                                                        >
+                                                                            <path
+                                                                                d="M42.1757 38.0741L43.1507 45.9738C43.4006 48.0488 41.1757 49.4987 39.4008 48.4237L28.926 42.1989C27.776 42.1989 26.6511 42.124 25.5511 41.974C27.4011 39.7991 28.501 37.049 28.501 34.0741C28.501 26.9743 22.3512 21.2246 14.7514 21.2246C11.8514 21.2246 9.17653 22.0495 6.95158 23.4994C6.87658 22.8744 6.85156 22.2494 6.85156 21.5995C6.85156 10.2247 16.7263 1 28.926 1C41.1257 1 51.0005 10.2247 51.0005 21.5995C51.0005 28.3493 47.5255 34.3242 42.1757 38.0741Z"
+                                                                                stroke="currentColor"
+                                                                                stroke-width="2"
+                                                                                stroke-linecap="round"
+                                                                                stroke-linejoin="round"
+                                                                            />
+                                                                            <path
+                                                                                d="M28.4993 34.0732C28.4993 37.0481 27.3994 39.7981 25.5494 41.9731C23.0745 44.973 19.1495 46.8979 14.7497 46.8979L8.22482 50.7727C7.12484 51.4477 5.72487 50.5228 5.87487 49.2478L6.49985 44.323C3.14994 41.9981 1 38.2731 1 34.0732C1 29.6733 3.34995 25.7984 6.94986 23.4985C9.17481 22.0485 11.8497 21.2236 14.7497 21.2236C22.3495 21.2236 28.4993 26.9734 28.4993 34.0732Z"
+                                                                                stroke="currentColor"
+                                                                                stroke-width="2"
+                                                                                stroke-linecap="round"
+                                                                                stroke-linejoin="round"
+                                                                            />
+                                                                        </svg>
+                                                                    </span>
+                                                                </div>
+                                                                <div
+                                                                    class="team__contact-wrapper"
+                                                                >
+                                                                    <!-- <p class="team-tel">
+                                    <i class="fa fa-phone"></i>
+                                    {{ dp.teamItems[4].phone }}
+                                  </p> -->
+                                                                    <p
+                                                                        class="team-email mt-20"
+                                                                        style="
+                                                                            overflow-wrap: anywhere;
+                                                                        "
+                                                                    >
+                                                                        {{
+                                                                            dp
+                                                                                .teamItems[5]
+                                                                                .email
+                                                                        }}
+                                                                    </p>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        <div
+                                                            class="team__content-10 text-center"
+                                                        >
+                                                            <h3
+                                                                class="team__title-10 pb-20"
+                                                            >
+                                                                {{
+                                                                    dp
+                                                                        .teamItems[5]
+                                                                        .prefix +
+                                                                    dp
+                                                                        .teamItems[5]
+                                                                        .firstname +
+                                                                    " " +
+                                                                    dp
+                                                                        .teamItems[5]
+                                                                        .surname
+                                                                }}
+                                                            </h3>
+                                                            <span
+                                                                class="team__designation-10"
+                                                            >
+                                                                <!-- {{ dp.teamItems[4].position }} -->
+                                                                <!-- <br /> -->
+                                                                {{
+                                                                    dp
+                                                                        .teamItems[5]
                                                                         .position_level
                                                                 }}
                                                             </span>
