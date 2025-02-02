21a3e0d20ae81acc58220b8b60fd337c4389e0b7
arnonr <arnonr@kmutnb.ac.th>
2025-02-02 10:20:18 +0700

    update

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,9 +1,12 @@
+import { useRouter } from "vue-router";
+
 export default defineNuxtRouteMiddleware((to, from) => {
     if (process.server) return; // กัน error บน server-side
 
     const token = localStorage.getItem("tp_token"); // ดึง Token จาก Storage
 
     if (!token) {
-        return navigateTo("/"); // ถ้ายังไม่ล็อกอินให้ไปหน้า Login
+        const router = useRouter();
+        router.replace("/"); // Redirect ไปหน้าใหม่โดยไม่เพิ่มลงใน history
     }
 });
