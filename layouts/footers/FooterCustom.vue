0c47699f31ef15d1a1038a7e9fe0e5169843f75c
arnonr <arnonr@kmutnb.ac.th>
2026-05-08 09:19:04 +0700

    refactor: format FooterCustom layout and add LINE social icon link

--- layouts/footers/FooterCustom.vue
+++ layouts/footers/FooterCustom.vue
@@ -1,16 +1,12 @@
 <template>
     <footer>
-        <div
-            :class="`footer__area ${style_2 ? 'footer__style-3' : ''}`"
-            :data-bg-color="`footer-bg-${style_2 ? 'grey-4' : 'black'}`"
-        >
+        <div :class="`footer__area ${style_2 ? 'footer__style-3' : ''}`"
+            :data-bg-color="`footer-bg-${style_2 ? 'grey-4' : 'black'}`">
             <div class="footer__top footer__top-5">
                 <div class="container">
                     <div class="row">
                         <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6">
-                            <div
-                                class="footer__widget footer__widget-5 mb-50 footer-col-5-1"
-                            >
+                            <div class="footer__widget footer__widget-5 mb-50 footer-col-5-1">
                                 <div class="footer__widget-content">
                                     <div class="footer__info">
                                         <h4>{{ t("TechnoPark") }}</h4>
@@ -30,46 +26,35 @@
                                                 {{ contact_phone }}
                                             </span>
                                             <br />
-                                            <span
-                                                >{{ t("Email") }} :
-                                                {{ contact_email }}</span
-                                            >
+                                            <span>{{ t("Email") }} :
+                                                {{ contact_email }}</span>
                                             <br />
                                             <br />
                                             <span> Social : </span>
-                                            <a
-                                                class="btn btn-sm btn-outline-primary me-2"
-                                                target="_blank"
-                                                href="https://www.facebook.com/KMUTNB.TECHNOPARK"
-                                            >
-                                                <i
-                                                    class="fab fa-facebook-f"
-                                                    style="
+                                            <a class="btn btn-sm btn-outline-primary me-2" target="_blank"
+                                                href="https://www.facebook.com/KMUTNB.TECHNOPARK">
+                                                <i class="fab fa-facebook-f" style="
                                                         font-size: 24px;
                                                         color: #4267b2;
-                                                    "
-                                                ></i>
+                                                    "></i>
                                             </a>
 
                                             <!-- ไอคอน YouTube -->
-                                            <a
-                                                class="btn btn-sm btn-outline-danger me-2"
-                                                target="_blank"
-                                                href="https://www.youtube.com/@KMUTNBTechnoPark99"
-                                            >
-                                                <i
-                                                    class="fab fa-youtube"
-                                                    style="
+                                            <a class="btn btn-sm btn-outline-success me-2" target="_blank"
+                                                href="https://www.youtube.com/@KMUTNBTechnoPark99">
+                                                <i class="fab fa-youtube" style="
                                                         font-size: 24px;
                                                         color: #ff0000;
-                                                    "
-                                                ></i>
+                                                    "></i>
                                             </a>
+
+                                            <!-- ไอคอน LINE -->
+                                            <a class="me-2" href="https://lin.ee/nEcoZel"><img
+                                                    src="https://scdn.line-apps.com/n/line_add_friends/btn/th.png"
+                                                    alt="เพิ่มเพื่อน" height="36" border="0"></a>
                                         </p>
 
-                                        <div
-                                            class="pl-10 pr-10 mt-1 d-inline-block bg-theme-primary footer-label"
-                                        >
+                                        <div class="pl-10 pr-10 mt-1 d-inline-block bg-theme-primary footer-label">
                                             {{ t("TechnoPark") }}
                                             {{ t("KMUTNB_FULLNAME") }}
                                         </div>
@@ -94,20 +79,13 @@
                         </div>
 
                         <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6">
-                            <div
-                                class="footer__widget footer__widget-5 mb-50 footer-col-5-3"
-                            >
+                            <div class="footer__widget footer__widget-5 mb-50 footer-col-5-3">
                                 <div class="footer__widget-content">
                                     <div class="footer__info">
                                         <iframe
                                             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3874.25103509604!2d100.50773527995139!3d13.823958961026323!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e29b9fec733295%3A0xabfe9c6fbd6d88d7!2zS01VVE5CIFRFQ0hOTyBQQVJLIOC4reC4uOC4l-C4ouC4suC4meC5gOC4l-C4hOC5guC4meC5guC4peC4ouC4tSDguKHguKvguLLguKfguLTguJfguKLguLLguKXguLHguKLguYDguJfguITguYLguJnguYLguKXguKLguLXguJ7guKPguLDguIjguK3guKHguYDguIHguKXguYnguLLguJ7guKPguLDguJnguITguKPguYDguKvguJnguLfguK0!5e0!3m2!1sen!2sth!4v1723620243600!5m2!1sen!2sth"
-                                            width="100%"
-                                            height="300"
-                                            style="border: 0"
-                                            allowfullscreen=""
-                                            loading="lazy"
-                                            referrerpolicy="no-referrer-when-downgrade"
-                                        ></iframe>
+                                            width="100%" height="300" style="border: 0" allowfullscreen=""
+                                            loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                                     </div>
                                 </div>
                             </div>
